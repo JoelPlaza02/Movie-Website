43c6b342e794ef8586c95b9cbb217b691c305b36
Joel Plaza <joel.plaza02@gmail.com>
2022-09-07 14:57:50 -0700

    fixed arrow slider

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 /* Arrow Movie Slider Section */
-const arrow = document.querySelectorAll('.arrow');
-const movieList = document.querySelectorAll('.movie-list');
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
 
-arrow.forEach((arrow, i) => {
-  const itemNumber = movieList[i].querySelectorAll('img').length;
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
   let clickCounter = 0;
-  arrow.addEventListener('click', () => {
+  arrow.addEventListener("click", () => {
     const ratio = Math.floor(window.innerWidth / 270);
     clickCounter++;
     if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
-      movieList[i].style.transform = `translateX(${
-        movieList[i].computedStyleMap().get("transform")[0].x.value - 450
-      }px)`;
+      movieLists[i].style.transform = `translateX(${-850}px)`;
     } else {
-      movieList[i].style.transform = "translateX(0)";
+      movieLists[i].style.transform = "translateX(0)";
       clickCounter = 0;
     }
   });
